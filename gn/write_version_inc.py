eb82c93d96da401ddafbc60312730d8d3c314fe5
Nico Weber <nicolasweber@gmx.de>
2018-08-27 09:56:16 -0400

    make write_version_inc.py check no @FOO@ strings remain, add missing @USE_Z3_SOLVER@= pattern

--- gn/write_version_inc.py
+++ gn/write_version_inc.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import sys
 
 def main():
@@ -13,6 +14,11 @@ def main():
     for from_to in args.from_to:
         from_, to = from_to.split('=', 1)
         output = output.replace(from_, to)
+
+    leftovers = re.findall('@[^@]+@', output)
+    if leftovers:
+        print 'missing patterns for', ', '.join(leftovers)
+        return 1
 
     if not os.path.exists(args.output) or open(args.output).read() != output:
         open(args.output, 'w').write(output)
